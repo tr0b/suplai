9aaebd96ad586f305925289ebe56eb9562e62a9b
tr0b <trobin3000600@gmail.com>
2020-02-19 14:59:43 -0600

    modificado schema de usuario (ingresado estado por defecto)

--- models/user.js
+++ models/user.js
@@ -2,13 +2,13 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const UserSchema = new Schema({
-  name: { type: String, required: true },
-  last_name: { type: String, required: true },
-  email: { type: String, required: true },
-  password: { type: String, required: true },
-  type: { type: String, required: true },
-  status: { type: String, required: true },
-  reqList: { type: ["#Solicitud#"], required: true }
+	name: { type: String, required: true },
+	last_name: { type: String, required: true },
+	email: { type: String, required: true },
+	password: { type: String, required: true },
+	type: { type: String, required: true },
+	status: { type: String, required: true, default: true },
+	reqList: { type: ["#Solicitud#"], required: true }
 });
 
 module.exports = mongoose.model("User", UserSchema);
