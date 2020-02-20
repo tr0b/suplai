f462a7b5bdee2f75f43e26d0eb054d91ad51bbb2
tr0b <trobin3000600@gmail.com>
2020-02-20 16:17:48 -0600

    small fix on the ../auth module route

--- routes/user.route.js
+++ routes/user.route.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
 const passport = require("passport");
-const { ensureAuthenticated } = require("../config/auth");
+const { ensureAuthenticated } = require("../auth");
 //User Model
 
 const User = require("./../models/user.js");
