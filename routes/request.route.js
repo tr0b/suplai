e76361db7e1408018d3437a95fc010900f107583
tr0b <trobin3000600@gmail.com>
2020-02-26 19:27:40 -0600

    Initial implementation of auth in request router

--- routes/request.route.js
+++ routes/request.route.js
@@ -1,6 +1,7 @@
 //WIP
 const express = require("express");
 const router = express.Router();
+const auth = require("../auth.js");
 //Request Model
 
 const Request = require("../models/request.js");
@@ -10,7 +11,8 @@ const Request = require("../models/request.js");
 const User = require("../models/user.js");
 //Add Product
 
-router.post("/add", (req, res) => {
+router.post("/add", auth, (req, res) => {
+	//Authentication implemented for request additions
 	let datecreated = new Date();
 	const { title, description, budget, status, datecreated } = req.body;
 	let errors = [];
@@ -26,6 +28,17 @@ router.post("/add", (req, res) => {
 			status: status,
 			datecreated: datecreated
 		});
+
+		//Save Request (TODO)
+		newRequest
+			.save()
+			.then(request => {
+				req.flash(
+					"success_msg",
+					"Success! Request successfully registered!"
+				);
+			})
+			.catch(err => console.log(err));
 		console.log(newRequest);
 		res.send("new Request Added");
 	}
