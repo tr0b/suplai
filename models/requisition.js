b174deb41dd659f36b2e11e2d82c71cfad957201
tr0b <trobin3000600@gmail.com>
2020-03-08 19:40:58 -0600

    Removed ShortId from Requisitions too

--- models/requisition.js
+++ models/requisition.js
@@ -1,9 +1,7 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
-const shortId = require("shortid");
 
 const RequisitionSchema = new Schema({
-	_id: { type: String, default: shortId.generate },
 	title: { type: String, required: true, trim: true },
 	description: { type: String, required: true, trim: true },
 	budget: { type: Number, required: true },
