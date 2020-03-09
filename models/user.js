39f2cfee858aeb118836dffe52debba2bcd019b7
Julian Diaz <jdiazh362@ulacit.ed.cr>
2020-03-08 19:13:59 -0600

    Adding ShortID

--- models/user.js
+++ models/user.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
+const shortId = require('shortid');
 const { Schema } = mongoose;
 
 const UserSchema = new Schema({
+	_id: {type: String, default: shortId.generate},
 	name: { type: String, required: true },
 	last_name: { type: String, required: true },
 	email: { type: String, required: true },
