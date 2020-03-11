9a946bfcd0bb7607694d269f42ae5557d8fd500b
Julian Diaz <jdiazh362@ulacit.ed.cr>
2020-03-11 00:04:22 -0600

    Fixing Passport Login Route API

--- routes/user.route.js
+++ routes/user.route.js
@@ -157,11 +157,15 @@ router.get("/logout", (req, res) => {
 });
 
 //[POST] Login User
-router.post("/login", (req, res, next) => {
-	passport.authenticate("local", {
-		successRedirect: "/yes",
-		failureRedirect: "/no"
-	})(req, res, next);
+router.post('/login',function(req,res,next){
+  passport.authenticate('local', function(err, user, info) {
+    if (err) { return res.status(501).json(err); }
+    if (!user) { return res.status(501).json(info); }
+    req.logIn(user, function(err) {
+      if (err) { return res.status(501).json(err); }
+      return res.status(200).json({message:'Login Success'});
+    });
+  })(req, res, next);
 });
 
 module.exports = router;
