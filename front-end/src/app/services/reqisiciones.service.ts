494c2d39e20533a5515344ed05eedbb5c360c2bb
Alejandro Salguero <alejandrosalgueroq@hotmail.com>
2020-03-18 00:09:15 -0600

    Creating requisitions crud

--- front-end/src/app/services/reqisiciones.service.ts
+++ front-end/src/app/services/reqisiciones.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
+import { RequisicionModel } from "../models/requesicion.model";
 
 @Injectable({
   providedIn: "root"
@@ -8,7 +9,23 @@ export class ReqisicionesService {
   constructor(private http: HttpClient) {}
 
   obtenerRequisiones() {
-  return this.http.get("http://localhost:3000/api/v1/requisitions",{withCredentials:true});
+    return this.http.get("http://localhost:3000/api/v1/requisitions", {
+      withCredentials: true
+    });
+  }
 
+  obtenerRequisicionPorID(id: string) {
+    return this.http.get("http://localhost:3000/api/v1/requisition/" + id, {
+      withCredentials: true
+    });
+  }
+
+  crearRequisicion(requisicion: RequisicionModel) {
+    return this.http.post("http://localhost:3000/api/v1/requisition", {
+      withCredentials: true,
+      title: requisicion.title,
+      description: requisicion.description,
+      budget: requisicion.budget
+    });
   }
 }
